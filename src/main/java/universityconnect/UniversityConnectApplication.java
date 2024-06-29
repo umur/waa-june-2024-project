ee846d295943f9b268cbf98165c0f5c54b0419b7
htetyadanar-hyo <htetyadanar.hyo@gmail.com>
2024-06-29 10:33:30 -0500

    upload project

---
+++ src/main/java/universityconnect/UniversityConnectApplication.java
@@ -0,0 +1,13 @@
+package universityconnecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityConnectApplication.class, args);
+	}
+
+}
