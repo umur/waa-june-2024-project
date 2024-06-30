2bec0a1f3a796e7866e03195c29a0ca3219f3fe5
Kush Raj Rimal <37412830+kushraj1204@users.noreply.github.com>
2024-06-30 14:50:02 -0500

    seed data added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,45 +1,53 @@
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('063cac59-0301-480f-a8ed-dcf7eeafa7ce'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SAC00000', 'Create Action', 'Create Action');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('8fd43131-4ed5-4f2f-a485-b706be655557'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SAC00001', 'Read Action', 'Read Action');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('47a2022c-fed0-45a0-afc4-66674a7e0ce4'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SAC00003', 'Update Action', 'Update Action');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('2c2ef853-7d2a-4e73-80ae-df0062fe195a'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SAC00004', 'Delete Action', 'Delete Action');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('c2d24bc5-1fe5-4f13-add5-a768d5bf4258'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SAC00002', 'Read Action List', 'Read Action List');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('68c5ea35-6a18-4049-8a0a-c0d8e17f3981'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SRO00000', 'Create Role', 'Create Role');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('991a8840-1039-415a-8e80-878c96aa6d19'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SRO00001', 'Read Role', 'Read Role');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('67895124-07fb-44a3-8ab7-7703a4316fa2'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SRO00003', 'Update Role', 'Update Role');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('4239435a-4fc8-4b35-b565-3c64403e9a7a'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SRO00004', 'Delete Role', 'Delete Role');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('b647869e-5f06-4d78-8946-97dcb03097f4'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SRO00002', 'Read Role List', 'Read Role List');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('f35b2b64-76e5-4821-ab1d-47eb7742ee45'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SUS00000', 'Create User', 'Create User');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('86ed352c-c77d-4899-a290-04af4ef01ce5'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SUS00001', 'Read User', 'Read User');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('e64b4e6c-3230-4327-9dba-2a7e5456ad0d'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SUS00003', 'Update User', 'Update User');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('136d2079-2b54-4e31-89f5-db09898c1be2'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SUS00004', 'Delete User', 'Delete User');
-INSERT INTO action
-(id, created_at, created_by, deleted, enabled, last_modified_at, last_modified_by, "version", code, description, "name")
-VALUES('5b2b13b4-d58b-483d-b5a0-39d9285c123d'::uuid, NULL, NULL, false, false, NULL, NULL, 0, 'SUS00002', 'Read User List', 'Read User List');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(1, NULL, NULL, NULL, NULL, 'create_user', 'Create System User', 'Create System User');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(2, NULL, NULL, NULL, NULL, 'view_user', 'Read System User', 'Read System User');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(3, NULL, NULL, NULL, NULL, 'modify_user', 'Update System User', 'Update System User');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(4, NULL, NULL, NULL, NULL, 'delete_user', 'Delete System User', 'Delete System User');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(5, NULL, NULL, NULL, NULL, 'view_user_list', 'Read System User List', 'Read System User List');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(6, NULL, NULL, NULL, NULL, 'create_action', 'Create new system actions', 'Create System Action');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(7, NULL, NULL, NULL, NULL, 'view_action', 'View existing system actions', 'Read System Action');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(8, NULL, NULL, NULL, NULL, 'modify_action', 'Modify system action details', 'Update System Action');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(9, NULL, NULL, NULL, NULL, 'delete_action', 'Delete system actions', 'Delete System Action');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(10, NULL, NULL, NULL, NULL, 'view_action_list', 'Read System action List', 'Read System Action List');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(11, NULL, NULL, NULL, NULL, 'create_role', 'Create new system roles', 'Create System Role');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(12, NULL, NULL, NULL, NULL, 'view_role', 'View existing system roles', 'Read System Role');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(13, NULL, NULL, NULL, NULL, 'modify_role', 'Modify system role details', 'Update System Role');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(14, NULL, NULL, NULL, NULL, 'delete_role', 'Delete system roles', 'Delete System Role');
+INSERT INTO `action`
+(id, created_at, created_by, last_modified_at, last_modified_by, code, description, name)
+VALUES(15, NULL, NULL, NULL, NULL, 'view_role_list', 'Read System role List', 'Read System Action List');
+
+INSERT INTO `role`
+(id, created_at, created_by, last_modified_at, last_modified_by, actions, code, description, name)
+VALUES(1, NULL, NULL, NULL, NULL, '["view_user_list", "view_action", "modify_user", "create_role", "delete_role", "modify_action", "view_role", "modify_role", "view_role_list", "create_action", "delete_action", "view_user", "create_user", "delete_user", "view_action_list"]', 'superuser', 'SuperUser', 'SuperUser');
+
+INSERT INTO `user`
+(id, created_at, created_by, last_modified_at, last_modified_by, email, enabled, password, username)
+VALUES(1, NULL, NULL, NULL, NULL, 'kushraj1204@gmail.com', 1, '$2a$10$zrX76Bn6VTIiJLbs2UNZzuo2CptsqFM6Lwm2vRvlncPBqB7ExsGZa', 'kush');
