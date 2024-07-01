2d9ee95f0e0285cecab819fe098a6311db5bdf9a
natboltu <madhusodanopu@gmail.com>
2024-07-01 14:01:26 -0500

    #event management basic crud operation

---
+++ src/main/java/edu/university_connect/controller/EventController.java
@@ -0,0 +1,61 @@
+package edu.university_connect.controller;
+
+import edu.university_connect.model.contract.dto.EventDto;
+import edu.university_connect.model.contract.request.event.EventRequest;
+import edu.university_connect.model.contract.response.ApiResponse;
+import edu.university_connect.model.enums.AppStatusCode;
+import edu.university_connect.service.MessagingService;
+import edu.university_connect.service.event.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/events")
+@CrossOrigin
+@RequiredArgsConstructor
+public class EventController {
+    private final EventService eventService;
+    private final MessagingService messagingService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<EventDto>>> getAll() {
+        String responseMessage = messagingService
+                .getResponseMessage(AppStatusCode.S20001, new String[]{"event"});
+        return ResponseEntity.ok(new ApiResponse<List<EventDto>>(responseMessage, eventService.getAll()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<EventDto>> getById(@PathVariable Long id) {
+        String responseMessage = messagingService
+                .getResponseMessage(AppStatusCode.S20001, new String[]{"event"});
+        return ResponseEntity.ok(new ApiResponse<>(responseMessage, eventService.getById(id)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Boolean>> delete(@PathVariable Long id) {
+        boolean response = eventService.delete(id);
+        String message = messagingService.getResponseMessage(AppStatusCode.S20005,
+                new String[]{"event"});
+        return ResponseEntity.ok(new ApiResponse<Boolean>(message, response));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<EventDto>> update(@Valid @RequestBody EventRequest eventRequest,
+                                                        @PathVariable Long id) {
+        EventDto updatedEvent = eventService.update(id, eventRequest);
+        String responseMessage = messagingService.getResponseMessage(AppStatusCode.S20004,
+                new String[]{"event"});
+        return ResponseEntity.ok(new ApiResponse<>(responseMessage, updatedEvent));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<EventDto>> create(@Valid @RequestBody EventRequest eventRequest) {
+        EventDto eventDto = eventService.create(eventRequest);
+        String message = messagingService.getResponseMessage(AppStatusCode.S20002, new String[]{"event"});
+        return ResponseEntity.ok(new ApiResponse<EventDto>(message, eventDto));
+    }
+}
---
+++ src/main/java/edu/university_connect/service/event/EventService.java
@@ -0,0 +1,20 @@
+package edu.university_connect.service.event;
+
+import edu.university_connect.domain.entity.Event;
+import edu.university_connect.model.contract.dto.EventDto;
+import edu.university_connect.model.contract.request.event.EventRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EventService {
+    List<EventDto> getAll();
+
+    EventDto getById(Long id);
+
+    boolean delete(Long id);
+
+    EventDto update(Long id, EventRequest eventRequest);
+
+    EventDto create(EventRequest eventRequest);
+}
