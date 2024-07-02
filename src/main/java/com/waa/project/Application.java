5fc51919bab92bd0bb9d4b8f80c20822d8550902
Suman Adhikari <ExpertKiD@users.noreply.github.com>
2024-07-02 13:43:48 -0500

    MERGE: MERGE Feat/students crud INTO develop (#5)
    * refactor/SHA-1: remove ModelMapperConfig
    
    * feat/SHA-1: add OpenAPIConfig
    
    * refactor/SHA-1: remove ROLE_ prefix from roles
    
    * refactor/SHA-1: update routes
    
    * refactor/SHA-1: remove context path
    
    * refactor/SHA-1: remove duplicate bean

--- src/main/java/com/waa/project/Application.java
+++ src/main/java/com/waa/project/Application.java
@@ -1,19 +1,15 @@
 package com.waa.project;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
     }
 }
