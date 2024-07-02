8fec929874997041c87a6c9e8c0dee7c807af3eb
AungKyawMyint <52407468+AungKyaw-Myint@users.noreply.github.com>
2024-07-02 14:36:06 -0500

    route update

--- src/main/java/com/waa/project/controller/DiscussionCategoryController.java
+++ src/main/java/com/waa/project/controller/DiscussionCategoryController.java
@@ -4,9 +4,11 @@ import com.waa.project.service.DiscussionCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class DiscussionCategoryController {
 
     @Autowired
--- src/main/java/com/waa/project/controller/DiscussionController.java
+++ src/main/java/com/waa/project/controller/DiscussionController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1")
 public class DiscussionController {
 
     @Autowired
