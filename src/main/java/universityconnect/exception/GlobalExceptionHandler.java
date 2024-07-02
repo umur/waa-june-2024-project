5d4061180e1b6e23540f6da153c9cfb625f5d4a9
gamhpang <gamhpang1812@gmail.com>
2024-07-02 15:49:26 -0500

    upload resource implemenation

--- src/main/java/universityconnect/exception/GlobalExceptionHandler.java
+++ src/main/java/universityconnect/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package universityconnect.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
