c270b2a0ee3d371124dad1b3f82b49be1f545515
Suman Adhikari <expertxkid@gmail.com>
2024-07-03 12:58:22 -0500

    feat/SHA-1: add MajorRepository

---
+++ src/main/java/com/waa/project/repository/MajorRepository.java
@@ -0,0 +1,8 @@
+package com.waa.project.repository;
+
+
+import com.waa.project.entity.Major;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MajorRepository extends JpaRepository<Major, Long> {
+}
