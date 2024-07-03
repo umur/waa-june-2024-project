b453e5ed510f9b2f65a9608f85d23fa2921cc75d
htetyadanar-hyo <htetyadanar.hyo@gmail.com>
2024-07-02 19:19:17 -0500

    add

--- src/main/java/universityconnect/controller/ResourceController.java
+++ src/main/java/universityconnect/controller/ResourceController.java
@@ -2,23 +2,31 @@ package universityconnect.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import universityconnect.dto.ResourceDTO;
 import universityconnect.service.ResourceService;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 
 @RestController
 @RequestMapping("/resources")
 @RequiredArgsConstructor
+@Slf4j
 public class ResourceController {
 
     private final ResourceService resourceService;
+    private final ResourceLoader resourceLoader;
 
     @PostMapping
-    public ResponseEntity<ResourceDTO> createResource(@RequestBody ResourceDTO resourceDTO){
+    public ResponseEntity<ResourceDTO> createResource(@RequestBody ResourceDTO resourceDTO) throws IOException {
         ResourceDTO resource = resourceService.createResource(resourceDTO);
         return new ResponseEntity<>(resource, HttpStatus.CREATED);
     }
