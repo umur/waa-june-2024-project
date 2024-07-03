89a6fc18816e3c1d695231b378fa8c55902affbe
htetyadanar-hyo <htetyadanar.hyo@gmail.com>
2024-07-03 10:04:06 -0500

    add search and filter feature

--- src/main/java/universityconnect/service/DiscussionService.java
+++ src/main/java/universityconnect/service/DiscussionService.java
@@ -1,7 +1,11 @@
 package universityconnect.service;
 
 import universityconnect.domain.Discussion;
+import universityconnect.domain.DiscussionSearchCriteria;
 import universityconnect.dto.DiscussionDTO;
+
+import org.springframework.data.domain.Pageable;
+import universityconnect.dto.DiscussionSearchResponseDTO;
 
 import java.util.List;
 
@@ -12,4 +16,5 @@ public interface DiscussionService {
     DiscussionDTO updateDiscussion(Long id, DiscussionDTO discussionDTO);
     void deleteDiscussion(Long id);
     Discussion findById(Long id);
+    public DiscussionSearchResponseDTO searchDiscussions(DiscussionSearchCriteria criteria, Pageable page);
 }
