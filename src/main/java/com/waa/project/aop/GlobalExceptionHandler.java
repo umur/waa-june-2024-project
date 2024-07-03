3dbd9928c60e8c66116f5589c756dd1cc4483399
Suman Adhikari <expertxkid@gmail.com>
2024-07-03 13:28:54 -0500

    feat/SHA-1: add HttpMessageNotReadableException and MethodArgumentNotValidException in GlobalExceptionHandler

--- src/main/java/com/waa/project/aop/GlobalExceptionHandler.java
+++ src/main/java/com/waa/project/aop/GlobalExceptionHandler.java
@@ -5,11 +5,17 @@ import com.waa.project.exception.ResourceAlreadyExistsException;
 import com.waa.project.exception.ResourceAlreadyInUseException;
 import com.waa.project.exception.ResourceNotFoundException;
 import com.waa.project.security.contract.ErrorResponse;
+import com.waa.project.security.contract.ValidationErrorResponse;
 import com.waa.project.security.exception.InvalidCurrentPasswordException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -52,4 +58,30 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleException(HttpMessageNotReadableException e) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException e) {
+        List<ValidationErrorResponse.FieldError> fieldErrors = e.getFieldErrors()
+                                                                .stream()
+                                                                .map(fieldError -> new ValidationErrorResponse.FieldError(
+                                                                        fieldError.getField(),
+                                                                        fieldError.getDefaultMessage()
+                                                                ))
+                                                                .toList();
+
+        ValidationErrorResponse errorResponse = new ValidationErrorResponse();
+        errorResponse.setErrors(fieldErrors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse.getErrorMap());
+    }
+
+
 }
