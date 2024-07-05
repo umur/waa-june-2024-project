b461e06ffe7726947d0c7a7347752a2e4973784b
Suman Adhikari <expertxkid@gmail.com>
2024-07-05 11:49:20 -0500

    feat/SHA-1: add updateStudentProfile() in StudentController

--- src/main/java/com/waa/project/controller/StudentController.java
+++ src/main/java/com/waa/project/controller/StudentController.java
@@ -2,24 +2,33 @@ package com.waa.project.controller;
 
 import com.waa.project.contracts.CreateStudentRequest;
 import com.waa.project.contracts.StudentResponse;
+import com.waa.project.contracts.UpdateStudentProfileRequest;
+import com.waa.project.security.exception.ActionForbiddenException;
+import com.waa.project.security.util.AuthErrorMessages;
 import com.waa.project.service.StudentService;
+import com.waa.project.service.impl.UserServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Validated
 @RestController
 @RequestMapping("/api/v1")
 public class StudentController {
 
-    private final StudentService studentService;
+    private final StudentService  studentService;
+    private final UserServiceImpl userServiceImpl;
 
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
+    public StudentController(StudentService studentService, UserServiceImpl userServiceImpl) {
+        this.studentService  = studentService;
+        this.userServiceImpl = userServiceImpl;
     }
 
 
@@ -41,5 +50,19 @@ public class StudentController {
                                                           ) {
         return new ResponseEntity<>(studentService.createStudent(createStudentRequest), HttpStatus.CREATED);
 
+    }
+
+    @PostMapping("/students/{username}/profile")
+    public ResponseEntity<StudentResponse> updateStudentProfile(
+            @RequestPart(value = "profile", required = false) @Valid UpdateStudentProfileRequest updateProfileRequest,
+            @RequestPart(value = "picture", required = false) MultipartFile picture,
+            @AuthenticationPrincipal User user,
+            @PathVariable("username") String username
+                                                               ) {
+        if (!username.equals(user.getUsername())) {
+            throw new ActionForbiddenException(AuthErrorMessages.forbidden());
+        }
+
+        return ResponseEntity.ok(studentService.updateStudentProfile(username, updateProfileRequest, picture));
     }
 }
