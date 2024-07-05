b25822bc1d17e8bb566fabb616ae22fbe8c4fe56
Suman Adhikari <expertxkid@gmail.com>
2024-07-05 11:25:49 -0500

    feat/SHA-1: add studentNotFound error message

--- src/main/java/com/waa/project/util/StudentErrorMessages.java
+++ src/main/java/com/waa/project/util/StudentErrorMessages.java
@@ -7,4 +7,8 @@ public class StudentErrorMessages {
     public static String studentNotFound(Long studentId) {
         return "Student with id #" + studentId + " not found.";
     }
+
+    public static String studentNotFound(String username) {
+        return "Student with username #" + username + " not found.";
+    }
 }
