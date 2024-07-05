b2fa705d50367364315053838318c7055e252a23
Suman Adhikari <expertxkid@gmail.com>
2024-07-05 11:13:04 -0500

    feat/SHA-1: add FileErrorMessages and FileSaveLocations

---
+++ src/main/java/com/waa/project/util/FileSaveLocations.java
@@ -0,0 +1,8 @@
+package com.waa.project.util;
+
+public class FileSaveLocations {
+
+    public static String studentProfile(String username) {
+        return "students/" + username + "/profile";
+    }
+}
