8abd23a3b59e672ea0e62ba818020f883b4ab619
AungKyawMyint <52407468+AungKyaw-Myint@users.noreply.github.com>
2024-07-06 17:00:57 -0500

    bug fix

--- src/main/java/com/waa/project/repository/DiscussionCommentsRepository.java
+++ src/main/java/com/waa/project/repository/DiscussionCommentsRepository.java
@@ -4,9 +4,12 @@ import com.waa.project.entity.DiscussionComments;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -25,5 +28,13 @@ public interface DiscussionCommentsRepository extends JpaRepository<DiscussionCo
     Page<DiscussionComments> findByParentCommentId(Long parentCommentId, Pageable pageable);
 
     void deleteAllByParentCommentId(DiscussionComments id);
+
+    List<DiscussionComments> findAllByDiscussionId(Long discussionId);
+
+    @Modifying
+    @Query("DELETE FROM DiscussionComments dc WHERE dc.parentCommentId.id IN :parentCommentIds")
+    void deleteAllByParentCommentIds(@Param("parentCommentIds") List<Long> parentCommentIds);
+
+    void deleteByDiscussionId(Long discussionId);
 
 }
