8bfd014338c25a7c18d01bc13aab7e7445c122bc
Suman Adhikari <expertxkid@gmail.com>
2024-07-13 17:31:54 -0500

    feat/security: add initial dashboard logic in Dashboard.jsx

--- frontend/src/features/dashboard/Dashboard.jsx
+++ frontend/src/features/dashboard/Dashboard.jsx
@@ -1,3 +1,31 @@
+import {useNavigate} from 'react-router';
+import {useEffect} from 'react';
+import {setTokens} from '../../core/setup/token';
+
 export default function Dashboard() {
-  return <>Dashboard Page</>;
+  var accessToken = localStorage.getItem('access-token');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(accessToken === null || accessToken === undefined || accessToken === '');
+
+    if (!accessToken) {
+      navigate('/login');
+    }
+  }, [accessToken]);
+
+  return (
+    <>
+      Dashboard Page
+      <button
+        onClick={() => {
+          setTokens();
+
+          navigate('/login');
+        }}
+      >
+        Logout
+      </button>
+    </>
+  );
 }
