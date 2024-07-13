bb60da9038291106a916c5b1951d1e2164185070
Suman Adhikari <expertxkid@gmail.com>
2024-07-13 17:29:09 -0500

    feat/security: add SuccessDialog.jsx and ErrorDialog.jsx

---
+++ frontend/src/core/component/dialogs/ErrorDialog.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Button, Modal} from 'react-bootstrap';
+
+const ErrorDialog = ({show, handleClose, errorMessage}) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Error</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{errorMessage}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ErrorDialog;
