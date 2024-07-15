efec246cc4bf8f1de35ac2ffaa88b45487d9ea0d
Suman Adhikari <expertxkid@gmail.com>
2024-07-15 10:26:07 -0500

    feat/security: add Students listing  page boilerplate

--- frontend/src/features/students/Students.jsx
+++ frontend/src/features/students/Students.jsx
@@ -1,3 +1,20 @@
+import {useNavigate} from 'react-router';
+import getCurrentProfile from '../../core/utils/current-profile';
+import {useEffect} from 'react';
+
 export default function Students() {
-  return <>Here lies list of students</>;
+  const navigate = useNavigate();
+  const profile = getCurrentProfile();
+
+  useEffect(() => {
+    if (profile === null || profile === undefined) {
+      navigate('/login');
+    }
+  }, [profile, navigate]);
+
+  return (
+    <>
+      <h3>Here lies list of students</h3>
+    </>
+  );
 }
