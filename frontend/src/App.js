2439ad728514f5d4552b01e17feaa76cc3d10248
phurpawangchuk <phurpawangchuk20@gmail.com>
2024-07-17 20:39:33 -0500

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,14 @@ import Login from './features/authentication/login/Login';
 import Register from './features/authentication/register/Register';
 import Students from './features/students/Students';
 import Events from './features/events';
-import { ProtectedRoute } from './features/authentication/protected-route/protectedRoute';
+import {ProtectedRoute} from './features/authentication/protected-route/protectedRoute';
 import EventDetails from './core/component/events/EventDetails';
 import StudentDetail from './features/students/student-detail';
 import UpdateStudentDetail from './features/students/update-student-detail';
+import Feedback from './features/feedbacks/Feedback';
+import AcademicResource from './features/academicResources/AcademicResource';
+import AcademicResourceType from './features/academicResourceTypes/AcademicResourceType';
+import FeedbackCategory from './features/feedbackCategories/FeedbackCategory';
 
 function App() {
   return (
@@ -17,11 +21,30 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/students" element={<Students />} />
-      <Route path="/events" element={<ProtectedRoute><Events /></ProtectedRoute>} />
-      <Route path="/events/:id" element={<ProtectedRoute><EventDetails /></ProtectedRoute>} />
+      <Route
+        path="/events"
+        element={
+          <ProtectedRoute>
+            <Events />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/events/:id"
+        element={
+          <ProtectedRoute>
+            <EventDetails />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/student/:username" element={<StudentDetail />} />
       <Route path="/profile/update" element={<UpdateStudentDetail />} />
       <Route path="/profile" element={<StudentDetail />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/feedbacks" element={<Feedback />} />
+      <Route path="/feedback-categories" element={<FeedbackCategory />} />
+      <Route path="/academic-resouce-type" element={<AcademicResourceType />} />
+      <Route path="/academic-resouces" element={<AcademicResource />} />
     </Routes>
   );
 }
